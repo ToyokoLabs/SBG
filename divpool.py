488f5dc362d2ec65771d636cd20bab01229da135
Sebastian Bassi <sbassi@genesdigitales.com>
2020-09-22 00:40:58 -0700

    allows command line argument for replics

--- divpool.py
+++ divpool.py
@@ -1,11 +1,11 @@
 import os
 import shutil
+import sys
 
 # divide pool
-# Root dir
 rootdir = 'all'
 cores = 32
-REPLIC = 1
+replic = sys.argv[1]
 
 def cpf(f, nd):
     shutil.copyfile(f, nd + os.sep + f)
@@ -13,7 +13,7 @@ def cpf(f, nd):
 # make core directories
 os.mkdir(rootdir)
 for core in range(cores):
-    new_dir = '{}{}{}-R{}'.format(rootdir, os.sep, core+1, REPLIC)
+    new_dir = '{}{}{}-R{}'.format(rootdir, os.sep, core+1, replic)
     os.mkdir(new_dir)
     cpf('leaprc.ff14SB', new_dir)
     cpf('runamber.py', new_dir)
@@ -22,7 +22,7 @@ for core in range(cores):
 
 core = 1
 for line in open('tripept.txt'):
-    mpr = '{}{}{}-R{}{}minipeprepo'.format(rootdir, os.sep, core, REPLIC, 
+    mpr = '{}{}{}-R{}{}minipeprepo'.format(rootdir, os.sep, core, replic, 
                                           os.sep)
     with open(mpr, 'a') as mprfh:
         mprfh.write(line)
