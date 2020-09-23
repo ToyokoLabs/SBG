c8b791748ac051de5c93f062a22c57c050236f53
Sebastian Bassi <sbassi@genesdigitales.com>
2020-09-22 18:37:53 -0700

    Generate bash file to upload all tgz

---
+++ uploadall.py
@@ -0,0 +1,18 @@
+# Generate bash file to upload all tgz
+
+import os
+
+# divide pool
+rootdir = 'all'
+cores = 32
+replic = 1
+
+# Generate bash script for uploading
+line1 = 'cd 1-R{}\ngsutil cp *.tar.gz gs://sbglabdata/tripep\n'.format(replic)
+newlines = ''
+for n in range(cores-1):
+    newlines+='cd ../{}-R{}\ngsutil cp *.tar.gz gs://sbglabdata/tripep\n'.format(n+2,replic)
+uploadlines = line1 + newlines
+
+with open('{}{}uploadall.sh'.format(rootdir, os.sep), 'w') as fout:
+    fout.write(uploadlines)
